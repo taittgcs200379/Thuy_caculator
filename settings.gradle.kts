e98bbe032d18eda500396d0bf06eb10f27e599e3
taittgcs200379 <taittgcs200379@fpt.edu.vn>
2023-10-26 17:46:10 +0700

    ok

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "caculator_CW2"
+include(":app")
+ 
